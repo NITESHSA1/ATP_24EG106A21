4b1a999c48f8a9271eab8c9c005c24a3e4138c05
NITESHSA1 <niteshgujjari123@gmail.com>
2026-05-04 15:03:46 +0530

    Update and rename assignment6.js to sum_of_nos_fun.js

--- session1_assignments/sum_of_nos_fun.js
+++ session1_assignments/sum_of_nos_fun.js
@@ -1,5 +1,7 @@
 /*6. write a fun that recevies an array as arg and return there sum*/
+//creating arry in js
 let arr=[22,21,6,33]
+//function for calculating all the array sums
 let lists=function (arr)
 {
     let sum=0

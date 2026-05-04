800485e027416ac138cfb78f5e3f1ff7629db00e
NITESHSA1 <niteshgujjari123@gmail.com>
2026-05-04 15:02:37 +0530

    Update and rename assignment5.js to largest_among_3_fun.js

--- session1_assignments/largest_among_3_fun.js
+++ session1_assignments/largest_among_3_fun.js
@@ -1,5 +1,6 @@
 /* 5.write a function that receives 3 number args and returns the largest number among them
 */
+//comparing all the numbers here with dunction
 let arr=function (a,b,c)
 {
     if(a>b && a>c)
@@ -14,4 +15,5 @@ let arr=function (a,b,c)
         return c
     }
 }
+//printing the output we use console.log
 console.log(arr(45,26,65))

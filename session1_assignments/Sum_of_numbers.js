bdd5f9a9fb3d54ed396b69e8fbfefc0cf73fa074
NITESHSA1 <niteshgujjari123@gmail.com>
2026-05-13 13:41:40 +0530

    Delete session1_assignments directory

--- session1_assignments/Sum_of_numbers.js
+++
@@ -1,9 +0,0 @@
-//find tht sum of marks in this array[98,75,65,98]
-let arr=[98,75,65,98]
-let sum=0
-//for loop in js
-for(let i=0;i<arr.length;i++)
-{
-    sum=sum+arr[i]
-}
-console.log(`sum of the array is ${sum}`)
